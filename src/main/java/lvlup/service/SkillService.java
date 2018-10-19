5d3303a848ac7b248775e8b92de374084e572b80
Meike <meike.hankewicz@gmail.com>
2018-10-19 11:55:01 +0200

    refactoring skillService

--- src/main/java/lvlup/service/SkillService.java
+++ src/main/java/lvlup/service/SkillService.java
@@ -34,9 +34,14 @@ public class SkillService {
         Skill skill = skillRepository.findOne(id);
         int currXp = skill.getCurr_xp() + xp;
         // check for level update
-        if(currXp >= 100 && skill.getCurr_lvl() < skill.getMax_lvl()){
-            skill.setCurr_lvl(skill.getCurr_lvl() + 1);
-            currXp -= 100;
+        if(currXp >= 100){
+            if(skill.getCurr_lvl() < skill.getMax_lvl()){
+                skill.setCurr_lvl(skill.getCurr_lvl() + 1);
+                currXp -= 100;
+            } else{
+                currXp = 100;
+                skill.setCurr_lvl(skill.getMax_lvl());
+            }
         }
 
         skill.setCurr_xp(currXp);
